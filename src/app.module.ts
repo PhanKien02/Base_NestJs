0a4be7b072c533a9e06c7442383db61cee66eb91
Phan Kien02 <phankien@11032002.com>
2024-12-04 22:24:34 +0700

    config project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import configuration from './configs/configuration';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformResponseInterceptor } from './interceptor/custom-response/custom-response.interceptor';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        UserModule,
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            isGlobal: true,
+            load: [configuration],
+        }),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: configuration().database.host,
+            port: configuration().database.port,
+            username: configuration().database.username,
+            password: configuration().database.password,
+            database: configuration().database.database,
+            entities: [__dirname + '/entity/*{.js,.ts}'],
+            synchronize: true,
+        }),
+        AuthModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService, {
+        provide: APP_INTERCEPTOR,
+        useClass: TransformResponseInterceptor,
+    },],
 })
-export class AppModule {}
+export class AppModule { constructor(private dataSource: DataSource) { } }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomExceptionFilter } from './interceptor/custom-exception/custom-exception.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+            credentials: true,
+        },
+    });
+    app.useGlobalFilters(new CustomExceptionFilter());
+    const config = new DocumentBuilder()
+        .setTitle('Note list example')
+        .setDescription('The note list API description')
+        .setVersion('1.0')
+        .addTag('Note list')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/document/swagger', app, documentFactory);
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
